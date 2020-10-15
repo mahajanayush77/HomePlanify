26ef80dcbac93154bf792d1dd7400196a906fb1e
Saksham Mittal <saksham1991999@users.noreply.github.com>
2020-10-16 00:41:47 +0530

    added onboarding screens

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+
+const kPrimaryColor = Color(0xFF6F35A5);
+const kPrimaryLightColor = Color(0xFFF1E6FF);
+
 const Color kPrimaryBackgroundColor = Colors.amber;
 
 const Color kAccentBackgroundColor = Colors.amberAccent;
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:housing/constant.dart';
 import 'package:housing/screens/news_page.dart';
+import 'package:housing/screens/onboarding/Login/login_screen.dart';
+import 'package:housing/screens/onboarding/Signup/signup_screen.dart';
+import 'package:housing/screens/onboarding/Welcome/welcome_screen.dart';
 import 'package:housing/screens/profile_page.dart';
 import 'screens/home_page.dart';
 import 'screens/list_view.dart';
@@ -21,7 +24,10 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => HomePage(),
+        '/': (context) => WelcomeScreen(),
+        '/welcome': (context) => WelcomeScreen(),
+        '/login': (context) => LoginScreen(),
+        '/signup': (context) => SignUpScreen(),
         '/properties': (context) => Search(),
         '/news': (context) => news_page(),
         '/add_property': (context) => news_page(),
