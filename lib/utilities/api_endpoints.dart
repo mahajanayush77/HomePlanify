d29ac2fb0f33fa5c1178b6b8f3f9c9ddcd4ecd8f
Saksham Mittal <saksham1991999@users.noreply.github.com>
2020-10-24 18:20:40 +0530

    utilities copy

---
+++ lib/utilities/api_endpoints.dart
@@ -0,0 +1,23 @@
+// All endpoint name must start with 'e'
+
+String get eLogIn => '/rest-auth/login/';
+String get eEmployeeProfile => '/api/employee/';
+String get eChangePassword => '/rest-auth/password/change/';
+String get eExamPreparingFor => '/api/exams-preparing-for/';
+String get eHigherEducation => '/api/higher-education/';
+String get eStudentAdmission => '/api/students/';
+String get eFAQS => '/api/faqs/';
+String get eEnquiry => '/api/enquiry/';
+String get eAddOffer => '/api/library-offers/';
+String get eReportABug => '/api/incidents/';
+String get eFeedback => '/api/feedback/';
+String get eFeedbackComplaints => '/api/library-feedback/';
+String get eFeedbackFollowUp => '/api/library-feedback-follow-up/';
+String get eMonthlyExpense => '/api/expense/monthly_expenses/';
+String get eExpense => '/api/expense/';
+String get eHoliday => '/api/holidays/';
+String get eLocker => '/api/library-locker/';
+String get eTimeSlot => '/api/time-slot/';
+String get eLibBranch => '/api/library-branch/';
+String get eSubscription => '/api/student-purchased-subscriptions/';
+String get eEnquiryFollowUp => '/api/enquiry-follow-up/';
